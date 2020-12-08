7c72a49eeff7bd4f784c66454da33d0b6d1f82d9
christopherc0825 <christopherc0825@gmail.com>
2020-12-08 17:19:35 -0500

    adding Critter Component

---
+++ ac-frontend/src/components/Critter.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react"
+
+const Critter = () => {
+
+    return (
+        <div>
+            <p>this is a critter</p>
+        </div>
+    )
+}
+
+export default Critter
